444594f5efc52ffbe2d7d8b8108e5f2a89e4525e
someone <someone@someplace.com>
2021-10-21 23:53:44 +0300

    solve11.2

--- my-app/src/components/question11.2/11.2.js
+++ my-app/src/components/question11.2/11.2.js
@@ -1,21 +1,43 @@
 import React from "react";
 
-export default function Check(){
+
+  
+class Check extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checked: false
+    };
+  }
+  
+ Chekedclick = ()=> {
+
+  document.querySelector('#checkbox3').this.state.checked = true;
+  document.querySelector('#checkbox4').this.state.checked = true;
+
+}
+render(){
   return(
     
     <div className='check'>
     
     Mockup:
     
-    <div className='checkbox'><input type="checkbox" id="checkbox" name="checkbox" value="a"/>I read the term of the app </div>
+    <div className='checkbox'><input type="checkbox" id="checkbox1" value="a"/>
+    <label htmlFor="checkbox1" >I read the term of the app</label> </div>
     
-    <input type="checkbox" id="checkbox" name="checkbox" value="b"/>I accept the term of the app
+    <div><input type="checkbox" id="checkbox2" value="b"/>
+    <label htmlFor="checkbox2" >I accept the term of the app</label></div>
     
-    <div><input type="checkbox" id="checkbox" name="checkbox" value="c"/>I want to get the weekly news letter</div>
+    <div><input type="checkbox" id="checkbox3" value="c" defaultChecked={!this.state.checked}/>
+    <label htmlFor="checkbox3" >I want to get the weekly news letter</label></div>
         
-    <input type="checkbox" id="checkbox" name="checkbox" value="d"/>I want to get sales and offers
-    
+    <div><input type="checkbox" id="checkbox4" value="d" defaultChecked={!this.state.checked}/>
+    <label htmlFor="checkbox4" >I want to get sales and offers</label></div>
+     
     </div>
   )
 }
-
+}
+export default Check;
